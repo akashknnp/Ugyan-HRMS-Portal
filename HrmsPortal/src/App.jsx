13cf8d26ae4e0506a6c38f97bafd719d22f9256a
Akash N N <akashnn2003@gmail.com>
2024-10-21 18:18:56 +0530

    Final push of the Reset component

--- HrmsPortal/src/App.jsx
+++ HrmsPortal/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import '../src/index.css'
+import '../src/reset.css'
 import React from 'react';
 import Reset from './components/Reset'
 import Login from './components/Login';
