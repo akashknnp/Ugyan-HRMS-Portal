3d03d96486ce2537df2f2e8246584ab9bacb0525
DanishSparky <danishsparkydt@gmail.com>
2024-11-06 15:55:04 +0530

    Clock-in and clock-out functions

--- Backend/Hrms_backend/Hrms_backend/urls.py
+++ Backend/Hrms_backend/Hrms_backend/urls.py
@@ -20,7 +20,6 @@ import Employee_Management.urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("",include('Employee_Management.urls')),
+    # path("",include('Employee_Management.urls')),
     path("",include('dashboard.urls'))
-
 ]
